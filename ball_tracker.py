8f0f9a412c1403fbf0fa63967860435ddbf5a714
Andrew Smith <aws2411@gmail.com>
2016-02-28 14:58:57 +0000

    calibration pitch 0

--- ball_tracker.py
+++ ball_tracker.py
@@ -24,7 +24,7 @@ class BallTracker(Tracker):
             [int] offset        how much to offset the coordinates
         """
         self.crop   = crop
-        self.color  = calibration[str(pitch)]['red']
+        self.color  = calibration['red']
         self.offset = offset
         self.name   = name
 
