c9e17175a609671026216a6a0eefbd9885b20fdc
jsren <jsrenwick3@outlook.com>
2016-03-01 02:50:24 +0000

    Refactored feed UI code into vision_feed

--- gui/vision_feed.py
+++ gui/vision_feed.py
@@ -9,21 +9,36 @@ try:
 except:
     pass
 
-from ..config import Configuration
+from colors import *
+from config import Configuration
+from math import radians, cos, sin, isnan
+from robot_tracker import ROBOT_DISTANCE
 
 def nothing(x): 
     pass
 
 class GUI:
 
+    x_ball_prev = 0
+    y_ball_prev = 0
+    counter = 0
+    x_ball_prev_prev = 0
+    y_ball_prev_prev = 0
+    x_ball = 0
+    y_ball = 0
+
     def __init__(self, pitch, color_settings, calibration, wrapper):
-        self.frame = None
-        self.pitch = pitch
+        self.frame          = None
+        self.pitch          = pitch
         self.color_settings = color_settings
-        self.wrapper = wrapper
+        self.wrapper        = wrapper
 
         self.calibration = calibration
         self.config      = Configuration.read_video_config(create_if_missing=True)
+
+        self.draw_robot     = True
+        self.draw_direction = True
+        self.draw_ball      = True
 
         # create GUI
         # The first numerical value is the starting point for the vision feed
@@ -55,8 +70,8 @@ class GUI:
             print "Colour:", self.frame[x, y], "@", (x, y)
 
 
-    def drawGUI(self, frame):
-        self.frame = frame
+    def update(self, wrapper):
+        self.frame = wrapper.frame
 
         if self.color_settings in [0, "small"]:
             attributes = ["bright", "contrast", "color", "hue", "Red Balance", "Blue Balance"]
@@ -68,6 +83,76 @@ class GUI:
         for att in attributes:
             self.config[att] = cv2.getTrackbarPos(att, 'frame2')
         self.config.commit()
+
+        if wrapper.draw_contours:
+            # Draw robot contours
+            for color in wrapper.world_contours.get('circles', list()):
+                contours = wrapper.world_contours['circles'][color]
+                cv2.fillPoly(self.frame, contours, BGR_COMMON[color])
+                cv2.drawContours(self.frame, contours, -1, BGR_COMMON['black'], 1)
+
+            # Draw ball contours
+            ball_contour = wrapper.world_contours.get('ball')
+            cv2.fillPoly(self.frame, ball_contour, BGR_COMMON['red'])
+            cv2.drawContours(self.frame, ball_contour, -1, BGR_COMMON['black'], 1)
+
+
+        # Feed will stop if this is removed and nothing else is shown
+        cv2.imshow('frame2', self.frame)
+
+        for r in wrapper.robots:
+            if not r.visible: continue
+
+            clx, cly, x, y = r.get_coordinates()
+            # TODO: Does age still apply? - jsren
+            if r.age > 0:
+                # Draw robot circles
+                if not isnan(clx) and not isnan(cly):
+                    if self.draw_robot:
+                        # Draw circle
+                        cv2.imshow('frame2', cv2.circle(self.frame, (int(clx), int(cly)),
+                                                        ROBOT_DISTANCE, BGR_COMMON['black'], 2, 0))
+
+                        # Draw Names
+                        cv2.imshow('frame2', cv2.putText(self.frame, r.name,
+                                                         (int(clx)-15, int(cly)+40),
+                                                         cv2.FONT_HERSHEY_COMPLEX, 0.45, (100, 150, 200)))
+
+                    if self.draw_direction:
+                        # Draw angle in degrees
+                        cv2.imshow('frame2', cv2.putText(self.frame, str(int(r.heading)),
+                                                         (int(clx) - 15, int(cly) + 30),
+                                                     cv2.FONT_HERSHEY_COMPLEX, 0.45, (100, 150, 200)))
+
+                        cv2.imshow('frame2', cv2.line(self.frame, (int(clx), int(cly)),
+                                                             (int(x), int(y)), BGR_COMMON['red'], 3, 0))
+
+                        # Draw line
+                        angle = r.heading
+                        new_x = clx + 30 * cos(radians(angle))
+                        new_y = cly + 30 * sin(radians(angle))
+                        cv2.imshow('frame2', cv2.line(self.frame, (int(clx), int(cly)),
+                                                             (int(new_x), int(new_y)),
+                                                      (200, 150, 50), 3, 0))
+
+        if self.draw_ball:
+            self.counter += 1
+            if 'ball' in wrapper.world_objects:
+                x_ball, y_ball = wrapper.world_objects['ball']
+                cv2.imshow('frame2', cv2.circle(self.frame, (int(x_ball), int(y_ball)), 8, (0, 0, 255), 2, 0))
+
+                if self.counter >= 5:
+                    self.x_ball_prev_prev = self.x_ball_prev
+                    self.y_ball_prev_prev = self.y_ball_prev
+                    self.x_ball_prev      = self.x_ball
+                    self.y_ball_prev      = self.y_ball
+                    counter = 0
+
+                cv2.imshow('frame2', cv2.arrowedLine(self.frame, (int(self.x_ball_prev_prev), int(self.y_ball_prev_prev)),
+                    (abs(int(x_ball+(5*(self.x_ball_prev-self.x_ball_prev_prev)))),
+                        abs(int(y_ball+(5*(self.y_ball_prev-self.y_ball_prev_prev))))),
+                    (0, 255, 0), 3, 10))
+
 
     def commit_settings(self):
         for attr in self.config:
--- vision_wrapper.py
+++ vision_wrapper.py
@@ -1,31 +1,21 @@
-from copy import deepcopy
-from math import radians, cos, sin, isnan
+
 
 import cv2
-import numpy as np
 
-from colors import *
+
 from gui.vision_feed import GUI
 from preprocessing.preprocessing import Preprocessing
-from robot_tracker import ROBOT_DISTANCE
-from util import RobotInstance, tools
+from util import RobotInstance
 from vision import Vision, Camera
-from config import Configuration, Calibration
+from config import Configuration
 
-x_ball_prev = 0
-y_ball_prev = 0
-counter = 0
-x_ball_prev_prev = 0
-y_ball_prev_prev = 0
-x_ball = 0
-y_ball = 0
+
 
 class VisionWrapper:
     """
     Class that handles vision
 
     """
-
     def __init__(self, pitch, color_settings, our_side, robot_details,
                  draw_GUI=False, pc_name=None, robots_on_pitch=list()):
         """
@@ -42,18 +32,6 @@ class VisionWrapper:
             [boolean] draw_GUI              Does not draw the normal image to leave space for GUI.
                                             Also forces the trackers to return circle contour positons in addition to robot details.
             [string] pc_name                Name of the PC to load the files from (BUT NOT SAVE TO). Will default to local machine if not specified.
-
-        Fields
-            pitch
-            camera
-            calibration
-            vision
-            postporcessing
-            color
-            side
-            preprocessing
-            model_positions
-            world_objects
         """
         pitch = int(pitch)
         assert pitch in [0, 1]
@@ -61,7 +39,8 @@ class VisionWrapper:
         self.pitch = pitch
         self.color_settings = color_settings
 
-        self.calibration = Configuration.read_calibration(machine_name=pc_name, create_if_missing=True)
+        self.calibration = Configuration.read_calibration(
+            pc_name, create_if_missing=True)
 
         # Set up camera for frames
         self.camera = Camera(pitch)
@@ -109,9 +88,9 @@ class VisionWrapper:
         from gui.common import MainWindow
         from gui.status import StatusUI
 
-        self.calibration_window = None
-        self.tracker_settings_window = None,
-        self.status_window = None
+        self.calibration_window      = None
+        self.tracker_settings_window = None
+        self.status_window           = None
 
         def create_windows():
             self.calibration_window = CalibrationUI(self.calibration)
@@ -139,7 +118,6 @@ class VisionWrapper:
 
     def saveCalibrations(self):
         Configuration.write_calibration(self.calibration)
-
 
     def get_robots_raw(self):
         # Filter robots that have no position
@@ -216,11 +194,6 @@ class VisionWrapper:
         """
         Gets this frame's positions from the vision system.
         """
-        global x_ball_prev,      y_ball_prev, \
-               x_ball_prev_prev, y_ball_prev_prev, \
-               x_ball,           y_ball, \
-               counter
-
         self.frame = self.camera.get_frame()
 
         # Apply preprocessing methods toggled in the UI
@@ -231,8 +204,8 @@ class VisionWrapper:
             cv2.imshow('bg sub', self.preprocessed['background_sub'])
 
         # Find object positions
-        # TODO: is this still the case?
         # model_positions have their y coordinate inverted
+        # TODO: is this still the case?
         self.world_objects, self.world_contours = \
             self.vision.perform_locate(self.frame)
 
@@ -248,69 +221,9 @@ class VisionWrapper:
                     if not robot.present: continue
 
                     if robot.update(r_data['clx'], r_data['cly'],
-                                    r_data['main_color'], r_data['side_color'], r_data['x'], r_data['y']):
+                                    r_data['main_color'], r_data['side_color'],
+                                    r_data['x'], r_data['y']):
                         break
 
-        if self.draw_GUI and self.draw_contours:
-            # Draw robot contours
-            for color in self.world_contours.get('circles', list()):
-                contours = self.world_contours['circles'][color]
-                cv2.fillPoly(self.frame, contours, BGR_COMMON[color])
-                cv2.drawContours(self.frame, contours, -1, BGR_COMMON['black'], 1)
-
-            # Draw ball contours
-            ball_contour = self.world_contours.get('ball')
-            cv2.fillPoly(self.frame, ball_contour, BGR_COMMON['red'])
-            cv2.drawContours(self.frame, ball_contour, -1, BGR_COMMON['black'], 1)
-
-
-        # Feed will stop if this is removed and nothing else is shown
-        cv2.imshow('frame2', self.frame)
-
-        for r in self.robots:
-            if not r.visible: continue
-
-            clx, cly, x, y = r.get_coordinates()
-            # TODO: Does age still apply? - jsren
-            if r.age > 0:
-                # Draw robot circles
-                if not isnan(clx) and not isnan(cly):
-                    if self.draw_robot:
-                        # Draw circle
-                        cv2.imshow('frame2', cv2.circle(self.frame, (int(clx), int(cly)), ROBOT_DISTANCE, BGR_COMMON['black'], 2, 0))
-
-                        # Draw Names
-                        cv2.imshow('frame2', cv2.putText(self.frame, r.name, (int(clx)-15, int(cly)+40), cv2.FONT_HERSHEY_COMPLEX, 0.45, (100, 150, 200)))
-
-                    if self.draw_direction:
-                        # Draw angle in degrees
-                        cv2.imshow('frame2', cv2.putText(self.frame, str(int(r.heading)), (int(clx) - 15, int(cly) + 30),
-                                                     cv2.FONT_HERSHEY_COMPLEX, 0.45, (100, 150, 200)))
-                        cv2.imshow('frame2', cv2.line(self.frame, (int(clx), int(cly)),
-                                                             (int(x), int(y)), BGR_COMMON['black'], 1, 0))
-
-                        # Draw line
-                        angle = r.heading
-                        new_x = clx + 30 * cos(radians(angle))
-                        new_y = cly + 30 * sin(radians(angle))
-                        cv2.imshow('frame2', cv2.line(self.frame, (int(clx), int(cly)),
-                                                             (int(new_x), int(new_y)), (200, 150, 50), 3, 0))
-
-        if self.draw_ball:
-            counter += 1
-            if 'ball' in self.world_objects:
-                x_ball, y_ball = self.world_objects['ball']
-                cv2.imshow('frame2', cv2.circle(self.frame, (int(x_ball), int(y_ball)), 8, (0, 0, 255), 2, 0))
-
-                if counter >= 5:
-                    x_ball_prev_prev = x_ball_prev
-                    y_ball_prev_prev = y_ball_prev
-                    x_ball_prev = x_ball
-                    y_ball_prev = y_ball
-                    counter = 0
-
-                cv2.imshow('frame2', cv2.arrowedLine(self.frame, (int(x_ball_prev_prev), int(y_ball_prev_prev)),
-                    (abs(int(x_ball+(5*(x_ball_prev-x_ball_prev_prev)))), abs(int(y_ball+(5*(y_ball_prev-y_ball_prev_prev))))), (0, 255, 0), 3, 10))
-
         if self.draw_GUI:
-            self.gui.drawGUI(self.frame)
+            self.gui.update(self)
